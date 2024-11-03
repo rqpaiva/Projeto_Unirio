40a5651a04c153dd5a4c38d1d474db62a61fb259
rqpaiva <144071319+rqpaiva@users.noreply.github.com>
2024-11-03 16:32:58 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -129,7 +129,7 @@ def home():
 
                 # Filtrar e extrair colunas
                 address_info = df['Address_client_dict'].apply(extract_address_info)
-                filtered_data = pd.concat([df[['status']], address_info], axis=1)
+                filtered_data = pd.concat([df[['status']], df[['created_at']] address_info], axis=1)
 
                 filtered_data.rename(columns={
                     'road': 'Rua',
