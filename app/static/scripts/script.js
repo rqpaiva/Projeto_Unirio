cc303a4b8f5fa7e2149838ea1564487100196e24
rqpaiva <144071319+rqpaiva@users.noreply.github.com>
2024-11-05 16:44:18 -0300

    Update script.js

--- app/static/scripts/script.js
+++ app/static/scripts/script.js
@@ -1,57 +1,76 @@
-document.addEventListener('DOMContentLoaded', function() {
+$(document).ready(function() {
     const fileInput = document.getElementById('fileInput');
     const fileLabel = document.querySelector('.custom-file-label');
     const textUpload = document.querySelector('.text-upload');
     const buttonSubimit = document.querySelector('.buttonSubimit');
-
     
     fileInput.addEventListener('change', function() {
         if (fileInput.files.length > 0) {
             textUpload.textContent = fileInput.files[0].name;
             fileLabel.classList.add('hidenButton');
-            buttonSubimit.classList.remove('hidenButton')
-
+            buttonSubimit.classList.remove('hidenButton');
         } else {
             fileLabel.textContent = 'Nenhum arquivo selecionado';
         }
     });
-});
 
-// Função para realizar a análise e exibir as novas tabelas
-$('#analyze-btn').on('click', function() {
-    $.ajax({
-        url: '/analyze',
-        type: 'POST',
-        success: function(response) {
-            if (response.general_info) {
-                $('#general-info').html(createTable(response.general_info));
-            }
-            if (response.missing_data) {
-                $('#missing-data').html(createTable(response.missing_data));
-            }
-            if (response.personal_info_table) {
-                $('#personal-info').html(createTable(response.personal_info_table));
-            }
-            if (response.temporal_analysis) {
-                let temporalHtml = '';
-                for (const [key, data] of Object.entries(response.temporal_analysis)) {
-                    temporalHtml += `<h4>${key}</h4>`;
-                    temporalHtml += createTable(data);
+    // Função para realizar a análise e exibir as novas tabelas
+    $('#analyze-btn').on('click', function() {
+        $.ajax({
+            url: '/analyze',
+            type: 'POST',
+            success: function(response) {
+                if (response.general_info) {
+                    $('#general-info').html(createTable(response.general_info));
                 }
-                $('#temporal-analysis').html(temporalHtml);
+                if (response.missing_data) {
+                    $('#missing-data').html(createTable(response.missing_data));
+                }
+                if (response.personal_info_table) {
+                    $('#personal-info').html(createTable(response.personal_info_table));
+                }
+                if (response.temporal_analysis) {
+                    let temporalHtml = '';
+                    for (const [key, data] of Object.entries(response.temporal_analysis)) {
+                        temporalHtml += `<h4>${key}</h4>`;
+                        temporalHtml += createTable(data);
+                    }
+                    $('#temporal-analysis').html(temporalHtml);
+                }
+                if (response.spatial_info_table) {
+                    $('#spatial-info').html(createTable(response.spatial_info_table));
+                }
+                if (response.comment_analysis) {
+                    $('#comment-analysis').html(createTable(response.comment_analysis));
+                }
+            },
+            error: function(xhr, status, error) {
+                alert(`Erro ao realizar a análise. Status: ${status}, Erro: ${error}`);
+                console.error("Detalhes da resposta:", xhr.responseText);
             }
-            if (response.spatial_info_table) {
-                $('#spatial-info').html(createTable(response.spatial_info_table));
-            }
-            if (response.comment_analysis) {
-                $('#comment-analysis').html(createTable(response.comment_analysis));
-            }
-        },
-        error: function(xhr, status, error) {
-            alert(`Erro ao realizar a análise. Status: ${status}, Erro: ${error}`);
-            console.error("Detalhes da resposta:", xhr.responseText);
-        }
+        });
     });
+
+    // Função para criar a tabela HTML a partir dos dados JSON
+    function createTable(data) {
+        var table = '<table class="table table-striped">';
+        table += '<thead><tr>';
+        Object.keys(data[0]).forEach(function(key) {
+            table += '<th>' + key + '</th>';
+        });
+        table += '</tr></thead><tbody>';
+
+        data.forEach(function(row) {
+            table += '<tr>';
+            Object.values(row).forEach(function(value) {
+                table += '<td>' + value + '</td>';
+            });
+            table += '</tr>';
+        });
+
+        table += '</tbody></table>';
+        return table;
+    }
 });
 
 
